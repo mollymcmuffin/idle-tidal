ccbb63e94f0244161cdb139053a72136acdda09d
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-07-02 12:06:30 +0800

    Setting up youtube api req

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -17,15 +17,14 @@ function Navbar() {
         onClick={menu}
         className="block w-6 h-6 cursor-pointer md:hidden"
         xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
+        strokeWidth="2"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           d="M4 6h16M4 12h16M4 18h16"
         />
       </svg>
--- components/Videos.js
+++ components/Videos.js
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Videos = () => {
-  return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/2fDzCWNS3ig?autoplay=1"
-      frameborder="0"
-      allow="autoplay; encrypted-media"
-      allowfullscreen=""
-    ></iframe>
-  );
+  const apiKey = "AIzaSyBE_fbE_ksu8Vn7kRST6pjYiMVpbSDea30";
+  const channelID = "UCf7cEcOrfw0tMSAutdUwoNQ";
+  const totalResults = 6;
+  const reqURL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&maxResults=${totalResults}&order=date&key=${apiKey}`;
+
+  const [videos, setVideos] = useState([]);
+  console.log(reqURL);
+
+  axios
+    .get(reqURL)
+    .then(function (response) {
+      // handle success
+      //console.log(response.data.items[0].id.videoId);
+      const videoResults = response.data.items;
+      videoResults.map((result) => result.id.videoId);
+
+      console.log(videoResults);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+      console.log("DONE REQ");
+    });
+
+  useEffect(() => {}, []);
+
+  return <div></div>;
 };
 
 export default Videos;
