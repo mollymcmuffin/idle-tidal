e6d495f5d9efc0fa08192045c300c3012e7d185a
zhengxuan <102131532+mollymcmuffin@users.noreply.github.com>
2022-07-03 09:39:02 +0800

    Added custom Document
    Added fonts link into doc.

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="http://fonts.cdnfonts.com/css/caviar-dreams"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.cdnfonts.com/css/super-retro-m54"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
